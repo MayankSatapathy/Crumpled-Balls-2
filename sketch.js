4cc78baa72b3019220655a0400c7733e4cd21d43
MayankSatapathy <72802125+MayankSatapathy@users.noreply.github.com>
2020-11-11 17:00:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball;
+var ground;
+var bin1,bin2,bin3,binImg;
+
+
+function preload() {
+	binImg = loadImage("dustbingreen.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	ball = new PaperBall();
+
+	ground = Bodies.rectangle(width/2,600,800,20,{isStatic: true});
+	World.add(world,ground);
+
+	bin1 = new Dustbin(660,580,150,20);
+	bin2 = new Dustbin(575,520,20,140);
+	bin3 = new Dustbin(745,520,20,140);
+
+	Engine.run(engine);
+	//console.log(ball);
+
+	//console.log(ball.body.position);
+}
+
+
+function draw() {
+	rectMode(CENTER);
+	background("cyan");
+	background.scale = 2.5;
+
+	ball.display();
+	
+	fill(255);
+	rect(ground.position.x,ground.position.y,800,20);
+
+	//bin1.display();
+	//bin2.display();
+	//bin3.display();
+
+	imageMode(CENTER);
+	image(binImg,665	,520,160,144);
+
+	drawSprites();
+
+	if(keyDown(UP_ARROW) && ball.body.position.y>530) {
+		Body.applyForce(ball.body,ball.body.position,{x:520,y:-540})
+	}
+
+	Engine.update(engine);
+}
+
